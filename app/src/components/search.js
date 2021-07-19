f2e7de738ef5c6643ae4b73393307d70d035bc72
Iman Bains <imanjitbains@yahoo.com>
2021-07-18 19:25:07 -0700

    Added search component

---
+++ app/src/components/search.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Search = (data) => {
+    return (
+        <form className="m-2">
+        <div className="input-group">
+          <input
+            onChange={data.handleChange}
+            value={data.search}
+            name="search"
+            type="text"
+            className="form-control"
+            placeholder="Search"
+            id="search"
+            />
+          <div className="input-group-append">
+          <button className="btn btn-light">
+            Search
+          </button>
+          </div>
+        </div>
+      </form>
+    )
+};
+
+
+export default Search;
